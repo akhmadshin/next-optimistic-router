092493d64b373e3da10c93688546627daf589beb
akhmadshin <qwasderfasder@gmail.com>
2024-05-28 12:01:35 +0700

    Fix usePageDataOptions

--- lib/usePageDataOptions.ts
+++ lib/usePageDataOptions.ts
@@ -5,7 +5,7 @@ import { GetRouteInfoProps, GetRouteInfoResponse, ModifiedRouter } from './route
 import { buildRoute } from './router-utils/build-route';
 import type { NextRouter, SingletonRouter } from 'next/router';
 
-export const usePageDataOptions = <T>(router: NextRouter, singletonRouter?: SingletonRouter) => {
+export const usePageDataOptions = <T>(router: NextRouter, singletonRouter: SingletonRouter) => {
   const { pathnameModifier } = useContext(OptimisticLinkContext);
 
   const queryFn = useCallback(async () => {
