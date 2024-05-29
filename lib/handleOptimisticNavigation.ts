cf1724d771562b7c18b2a6d5d8339d70ce9b6221
akhmadshin <qwasderfasder@gmail.com>
2024-05-29 16:50:41 +0700

    Fix handleOptimisticNavigation signature

--- lib/handleOptimisticNavigation.ts
+++ lib/handleOptimisticNavigation.ts
@@ -1,9 +1,10 @@
 import { isLocalURL } from 'next/dist/shared/lib/router/utils/is-local-url';
 import { ModifiedRouter } from './router-extensions/types.ts';
 import { SingletonRouter } from 'next/router';
+import type { Url } from 'next/dist/shared/lib/router/router';
 import { formatWithValidation } from 'next/dist/shared/lib/router/utils/format-url';
 
-export const handleOptimisticNavigation = (href: string, singletonRouter: SingletonRouter) => {
+export const handleOptimisticNavigation = (href: Url, singletonRouter: SingletonRouter) => {
   const urlAsString = typeof href === 'string' ? href : formatWithValidation(href)
   const isLocal = isLocalURL(urlAsString);
 
