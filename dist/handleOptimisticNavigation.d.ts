ee3e462a77b0eafad72301d72353f9bb4741d03a
akhmadshin <qwasderfasder@gmail.com>
2024-05-29 16:54:22 +0700

    Minor fixes

--- dist/handleOptimisticNavigation.d.ts
+++ dist/handleOptimisticNavigation.d.ts
@@ -1,3 +1,4 @@
 import { SingletonRouter } from 'next/router';
+import { Url } from 'next/dist/shared/lib/router/router';
 
-export declare const handleOptimisticNavigation: (href: string, singletonRouter: SingletonRouter) => void;
+export declare const handleOptimisticNavigation: (href: Url, singletonRouter: SingletonRouter) => void;
